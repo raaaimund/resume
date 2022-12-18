d770fe3247aaf44383e9e9957f95d5d955d4849d
raaaimund <raimund.rittnauer@gmail.com>
2022-12-18 13:48:15 +0100

    added caching for assets

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    headers: async () => [
+        {
+            source: '/:all*(svg|jpg|png)',
+            locale: false,
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'public, max-age=604800, stale-while-revalidate',
+                },
+            ],
+        },
+    ],
 }
 
 module.exports = nextConfig
