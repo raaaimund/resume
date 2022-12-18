ef3d2cb8af35eaf3f2c06e721c547d76399effb7
raaaimund <raimund.rittnauer@gmail.com>
2022-12-18 13:42:54 +0100

    used React.Image instead of img

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface Props {
     imgSrc: string,
@@ -7,8 +8,7 @@ interface Props {
 
 export default function ProfileImage(props: Props) {
     return (
-        <img className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-             src={props.imgSrc}
-             alt={props.alt}/>
+        <Image src={props.imgSrc} alt={props.alt} width={"200"} height={"200"}
+               className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"/>
     )
 }
